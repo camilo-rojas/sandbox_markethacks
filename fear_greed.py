ebe760b12532ea245fa1bde61734bee9318565ab
Camilo Rojas <camilo.rojas@gmail.com>
2021-02-03 19:33:10 -0500

    Fear and greed index demo

---
+++ fear_greed.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://money.cnn.com/data/fear-and-greed/"
+token = "Fear &amp; Greed Now: "
+fear = 20
+greed = 80
+
+
+class get_fear_geed_index():
+    def __init__(self, *args, **kwargs):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, features='html.parser')
+        div = soup.find_all(id='needleChart')
+        div = str(div[0])
+        position = div.find(token)+len(token)
+        self.fear_greed_index = int(div[position:position+3])
+
+
+print(get_fear_geed_index().fear_greed_index)
